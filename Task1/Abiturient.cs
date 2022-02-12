f605aa4f406574f8fd35dec723129248d1b9b64c
une pomme <AstafevK@yandex.ru>
2022-02-12 18:17:19 +0300

    LabWork7

---
+++ Task1/Abiturient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Abiturient
+    {
+        private string fullName;
+        private int baseClass;
+        private int avg;
+
+
+        public int Value { get; set; }
+
+        public static Abiturient operator +(Abiturient abiturient)  // 1 zad
+        {
+            abiturient.avg++;
+            return abiturient;
+        }
+
+        public static Abiturient operator +(Abiturient abiturient1, Abiturient abiturient2) // 2 zad
+        {
+            Abiturient abiturient3 = new Abiturient();
+            abiturient3.fullName = abiturient1.fullName;
+            abiturient3.baseClass = abiturient1.baseClass;
+            abiturient3.avg = abiturient1.avg + abiturient2.avg;
+            return abiturient3;
+        }
+
+        public static bool operator ==(Abiturient abiturient1, Abiturient abiturient2) // task 3
+        {
+            return abiturient1.avg == abiturient2.avg && abiturient1.fullName == abiturient2.fullName && abiturient1.baseClass == abiturient2.baseClass;
+        }
+
+        public static bool operator !=(Abiturient abiturient1, Abiturient abiturient2)
+        {
+            return abiturient1.avg != abiturient2.avg || abiturient1.fullName != abiturient2.fullName || abiturient1.baseClass != abiturient2.baseClass;
+        }
+
+        public static bool operator true(Abiturient abiturient2) // task 4
+        {
+            return abiturient2.avg >= 0;
+        }
+
+        public static bool operator false(Abiturient abiturient2) 
+        {   
+            return abiturient2.avg < 0; 
+        }
+
+
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Abiturient c1 = new Abiturient { Value = 20 };
+            Abiturient c2 = new Abiturient { Value = 23 };
+
+           // Class c3 = c1 + c2;
+            Console.WriteLine(Value.c3);
+        }
+    }
+}
